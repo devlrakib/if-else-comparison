5206ebdb3b734434a6ca140758825269b8beb2ec
devlrakib <devrakib5@gmail.com>
2024-03-14 01:28:42 +0600

    first commit

---
+++ conditional.js
@@ -0,0 +1,15 @@
+// var iphonePrice = 79000;
+// var mybudget    = 9500;
+// if (iphonePrice>mybudget){
+//     console.log("This is Right Qoute");
+// };
+
+
+var eng = 50;
+var ban = 60;
+if(eng>ban){
+    console.log("This is Our Language");
+}
+else{
+ console.log("This is right place Now")
+}
